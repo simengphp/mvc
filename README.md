6bcdb8458aed87d0e51c1dc8242847216b3c09ba
刘柱 <476319748@qq.com>
2018-03-04 10:12:31 +0800

    整合了memcache-crazy-03-04

--- README.md
+++ README.md
@@ -21,6 +21,10 @@ extend==扩展
 3、模板引擎使用的是smarty
 
 
-4、整合了memcache-03-04
-
+4、整合了memcache->memcachedExtend.php
+memcache讲解：
+1)memcache占用内存比较严重，如果你是单服务架构那么不适合使用memcache,它会抢占内存
+2)不能持久化操作，只适合一些为了缓存为应用的服务
+3)支持的类型有限
+memcached比memcache有更加丰富的对数据缓存的操作
 未来：各种模式的设计添加
--- extend/MemcachedExtend.php
+++ extend/MemcachedExtend.php
@@ -13,9 +13,28 @@ class MemcachedExtend {
     public function memTest(){
         $mem = new Memcache();
         $mem->connect("127.0.0.1",11211);
-        $mem->set("string","刘柱");
-        $string = $mem->get("string");
+//        $mem->set("string","刘柱");
+//        $string = $mem->get("string");
+
+//        echo "<hr />统计服务器池中的所有服务器状态";
+//        echo "<pre>";
+//        var_dump($mem->getextendedstats());
+//        echo "<hr />获取某一个服务器的状态";
+//        var_dump($mem->getserverstatus("127.0.0.1"));
+//        echo "<hr />";
+//        var_dump($mem->getstats());
+//        echo "<hr />获取版本信息";
+//        var_dump($mem->getversion());
+
+        $arr = [1,2,3,4,5,666];
+//        $mem->set("arr",$arr);
+        //替换某个缓存变量的值
+        $json = json_encode($arr);
+        $mem->replace("arr",$json);
+        $string = $mem->get("arr");
         var_dump($string);
+
+
     }
 }
 
